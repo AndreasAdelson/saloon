3b922e092727c01c122c99033e929b7539658e23
Andreas <andreasadelson@gmail.com>
2023-04-03 10:57:07 +0200

    Footer + tarifs WIP

--- wp-content/themes/hello-elementor-child/fonction.php
+++ wp-content/themes/hello-elementor-child/fonction.php
@@ -6,3 +6,4 @@ function childtheme_parent_styles() {
  wp_enqueue_style( 'parent', get_template_directory_uri().'/style.css' );                       
 }
 add_action( 'wp_enqueue_scripts', 'childtheme_parent_styles');
+add_filter( 'elementor_pro/dynamic_tags/shortcode/should_escape', '__return_false' ); 
--- wp-content/uploads/custom-css-js/1516.js
+++ wp-content/uploads/custom-css-js/1516.js
@@ -24,5 +24,45 @@ document.addEventListener("DOMContentLoaded", function(){
 	}
 	  
 });
+var intervalId = 0;
+var changeBackgroundImage = function(elementTabId){
+	const forfaitSection = document.getElementById("forfait-section");
+	let imagesTab = ["url(http://localhost/saloon/wp-content/uploads/2023/04/400A7001PHOTOS-RETOUCHER-copie-scaled.jpg)", "url(http://localhost/saloon/wp-content/uploads/2023/04/400A7049PHOTOS-RETOUCHER2-scaled.jpg)", "url(http://localhost/saloon/wp-content/uploads/2023/04/400A7093PHOTOS-RETOUCHER-scaled.jpg)"];
+		switch (elementTabId) {
+		  case 'elementor-tab-title-2081':
+			clearInterval(intervalId);
+			forfaitSection.style.backgroundImage = "url(http://localhost/saloon/wp-content/uploads/2023/03/black-and-white-2558273_1920.jpg)";
+			break;
+		  case 'elementor-tab-title-2082':
+			forfaitSection.style.backgroundImage = imagesTab[0];
+			var i = 1;
+			intervalId = setInterval(async() => {
+				if (i > 2) {
+					i = 0;
+				}
+				forfaitSection.style.backgroundImage = imagesTab[i];
+				i++
+			}, 5000);
+			break;
+		  case 'elementor-tab-title-2083':
+			clearInterval(intervalId);
+			forfaitSection.style.backgroundImage = "url(http://localhost/saloon/wp-content/uploads/2023/04/400A7136PHOTOS-RETOUCHER22-scaled.jpg)";
+			break;
+		  default:
+			clearInterval(intervalId);
+			forfaitSection.style.backgroundImage = "url(http://localhost/saloon/wp-content/uploads/2023/04/400A7001PHOTOS-RETOUCHER-copie-scaled.jpg)";
+		}
+}
+
+window.onload = function()
+{
+   jQuery(".elementor-tab-title").on("click", function(){ 
+   		var $this = jQuery(this);
+	    var elementTabId = $this.attr('id');
+		changeBackgroundImage(elementTabId);
+	});
+}
+
+
 </script>
 <!-- end Simple Custom CSS and JS -->
